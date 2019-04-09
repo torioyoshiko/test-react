bb5f579f1b2354ebeb516b1464e56cad0ebb6f51
Torio Yoshiko <torio.yoshiko@gmail.com>
2019-04-09 10:09:10 +0300

    third
    trying to fix handleChange

--- src/components/createProject.js
+++ src/components/createProject.js
@@ -1,16 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import "./css/createProject.css"
 
 class CreateProject extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+        console.log(this.state)
+    }
+
+    handleChange2(event) {
+        this.setState({value: event.target.value});
+        console.log(this.state)
+    }
+
+    handleSubmit(event) {
+        console.log("form was submitted");
+    }
+
     render() {
         return (
             <div className="create-project">
                 <h2>Start Project</h2>
                 <p>Project name</p>
-                <input/>
+                <input onChange={this.handleChange}/>
                 <p>Brief</p>
-                <input/>
-                <button>Create</button>
+                <input onChange={this.handleChange2}/>
+                <button onClick={this.handleSubmit}>Create</button>
             </div>
         )
     }
